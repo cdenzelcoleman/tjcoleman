625a9b2c1c9e254e4f1699d88e6c281a9d089cdf
cdenzelcoleman <camdcole@gmail.com>
2025-06-29 19:42:27 -0500

    Add favicon handler

--- backend/videos/urls.py
+++ backend/videos/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from rest_framework.routers import DefaultRouter
 from .views import VideoViewSet, BlogPostViewSet, admin_login, admin_logout, check_admin_status, get_csrf_token, debug_session, subscribe_email, unsubscribe_email
 
@@ -14,12 +14,16 @@ def home(request):
         }
     })
 
+def favicon(request):
+    return HttpResponse(status=204)
+
 router = DefaultRouter()
 router.register(r'videos', VideoViewSet)
 router.register(r'blog-posts', BlogPostViewSet, basename='blogpost')
 
 urlpatterns = [
     path('', home, name='home'),
+    path('favicon.ico', favicon, name='favicon'),
     path('api/', include(router.urls)),
     path('api/admin/login/', admin_login, name='admin_login'),
     path('api/admin/logout/', admin_logout, name='admin_logout'),
